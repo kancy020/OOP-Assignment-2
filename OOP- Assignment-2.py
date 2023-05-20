98b51b7334eda1a0e101f8a78f6cfd1fa97c1588
kancy020 <115684569+kancy020@users.noreply.github.com>
2023-05-20 16:08:07 +0930

    put in place holder code
    filled in each class and their attributes plus methos. plus i added additional notes so I know how to continue on each class and method

--- OOP- Assignment-2.py
+++ OOP- Assignment-2.py
@@ -1,29 +1,144 @@
-from abc import ABC 
-import Materials
 
-class Crafter(ABC):
+class Workshop:
+     pass
+    
+    #displays weapon, use if to see if value == to enchated opr not enchanted weapon
+    def displayWeapons():
+        pass
+    
+    #prints all enchatments in an enchantments list   
+    def displayEnchantments():
+        pass
+    
+    #prints material name and amount of material remaining
+    def displayMaterials():
+     
+#is an abstract class    
+class Crafter:
+    pass
 
+    #crafting method for forge and enchanter(add weapon or enchantment to list)
     def craft():
         pass
-
+    
+    #disassemble method for forge and enchanter(removes weapon or enchantment from list)
     def disassemble():
         pass
 
-class Weapon():
-    def __init__(self, weaponName, material):
-        self.weaponName = weaponName
-        self.material = material
-        
-    def displayInfo(self):
-        print('Weapon name is ' + self.weaponName +' material is', self.material.__class__.__name__, 'strength is', self.material.strength)
-        
-sword = Weapon('sword', Materials.Wood(5))   
-anotherweapon = Weapon('bow', Materials.Metal(4,5))  
-weapon = {}
+class Enchanter(Crafter):
+    pass
+    # copy dictionary from assignment sheet
+    
+    #crafting method for forge and enchanter(add weapon or enchantment to list)
+    def craft():
+        pass
+    
+    #disassemble method for forge and enchanter(removes weapon or enchantment from workshop)
+    def disassemble():
+        pass
+    
+    def enchant(weapon, enchantmentName, enchantment):
 
-weapon['weapon 2'] = anotherweapon
-weapon['weapon 1'] = sword   
 
-for name in weapon.keys():
-    if name == 'weapon 1':
-        print(name)
+class Forge(Crafter):
+        pass
+
+    #crafting method for forge and enchanter(add weapon or enchantment to list)
+    def craft():
+        pass
+    
+    #disassemble method for forge and enchanter(removes weapon or enchantment from list)
+    def disassemble():
+        pass
+
+# uses mateirla to craft
+class Enchantment:
+    def __init(self,name, magicDamage, effect, primaryMaterial, secondaryMaterial):
+            self.__name = name
+            self.__magicDamage = magicDamage
+            self.__effect = effect
+            self.__primaryMaterial = primaryMaterial
+            self.__secondaryMaterial = secondaryMaterial
+            
+    def getEnchantmentName():
+        pass
+    
+    def getMagicDamage():
+        pass
+    
+    def setMagicDamage():
+        pass
+    
+    def getEffect():
+        pass
+    
+    def setEffect():
+        pass
+    
+    def getPrimaryMaterial():
+        pass
+        
+    def getSecondaryMaterial():
+        pass
+
+    def calculateMagicDamage():
+        pass
+            
+    def useEffect():
+        pass
+            
+    #property to be made for each
+            
+    pass
+
+class Weapon:
+    def __init__(self,name, damage, primaryMaterial, secondaryMaterial):
+         self.__name = name
+         self.__damage = damage
+         self.__primaryMaterial = primaryMaterial
+         self.__secondaryMaterial = secondaryMaterial
+         self.__enchanted = None
+         
+         
+    # get weapon name  
+    def getName():
+        pass
+    
+    def setName():
+        pass
+    
+    #get damage rating of weapon
+    def getDamage():
+        pass
+    
+    def setDamage():
+        pass
+    
+    #do a boolean to see if it is enchated
+    def getEnchanted():
+        pass
+    
+    def setEnchanted():
+        pass
+    
+    def getPrimaryMaterial():
+        pass
+        
+    def getSecondaryMaterial():
+        pass
+    
+    #prints the attached enchantment to the weapon
+    def getEnchantment():    
+        pass
+    
+    def setEnchantment():
+        pass
+    
+    
+    #check type fo materials that the weapon is made out of, and returns in a float what  thwe weapon damage is
+    def calculateDamage():
+        pass
+    
+    def attack():
+        pass
+    #property to be made for each
